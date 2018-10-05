ca150b9a76c15fada044b568201c7df1c09054f8
Hanzhi Zhou <hanzhi713@163.com>
2018-10-05 15:44:44 -0400

    Updated README and chap4_exercises Added .gitignore

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # lean-proofs
 
-Logics and proofs in Lean prover.
+Logics and proofs in Lean.
 
 ## About
 
 This repo contains the lean proofs that I explored and completed during the course of CS2102 at UVa.
 
-[Lessons folder](lessons) contains my class notes (basically the random mess that I typed in class). 
+[Lessons folder](lessons) contains my class notes, which are basically the random mess that I typed in class.
 
-I'm also managing to complete lean exercises at the end of each chapter in the openbook [Theorem Proving in Lean](https://leanprover.github.io/theorem_proving_in_lean/). My answers are in to [chapter exercises](chapter_exercises) directory. I'll try to complete the chapter exercises after that chapter is taught in class.
+I'm also working on the lean exercises at the end of each chapter in the openbook [Theorem Proving in Lean](https://leanprover.github.io/theorem_proving_in_lean/). My answers are in the [chapter exercises](chapter_exercises) directory.
--- chapter_exercises/chap4_exercises.lean
+++ chapter_exercises/chap4_exercises.lean
@@ -2,7 +2,24 @@
 
 variables (α : Type) (p q : α → Prop)
 
-example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) :=
+begin
+  apply iff.intro,
+    assume first,
+    apply and.intro,
+      assume x,
+      show p x, from (first x).1,
+
+      assume x,
+      show q x, from (first x).2,
+    
+    assume second,
+    assume x,
+    apply and.intro,
+      show p x, from second.1 x,
+      show q x, from second.2 x
+end
+
 example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := 
 begin
     assume first,
@@ -10,7 +27,15 @@ begin
     assume a,
     exact first a (second a),
 end
-example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
+
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x :=
+begin
+  assume first,
+  assume x,
+  cases first with px qx,
+    show p x ∨ q x, from or.inl (px x),
+    show p x ∨ q x, from or.inr (qx x)
+end
 
 -- Exercise 2
 -- It is often possible to bring a component of a formula outside a universal quantifier, when it does not depend on the quantified variable. Try proving these (one direction of the second of these requires classical logic):
@@ -21,13 +46,32 @@ example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
 example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
 
 -- Exercise 3
--- It is often possible to bring a component of a formula outside a universal quantifier, when it does not depend on the quantified variable. Try proving these (one direction of the second of these requires classical logic):
-example : α → ((∀ x : α, r) ↔ r) := sorry
-example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
-example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
+-- Consider the “barber paradox,” that is, the claim that in a certain town there is a (male) barber that shaves all and only the men who do not shave themselves. Prove that this is a contradiction:
+variables (men : Type) (barber : men)
+variable  (shaves : men → men → Prop)
 
--- Exercise 4
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) :
+  false := sorry
 
+-- Exercise 5
+open classical
+variable a : α
+
+example : (∃ x : α, r) → r := sorry
+example : r → (∃ x : α, r) := sorry
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := sorry
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
+
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := sorry
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
+example : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
+example : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
+
+-- Exercise 6
 variables (real : Type) [ordered_ring real]
 variables (log exp : real → real)
 variable  log_exp_eq : ∀ x, log (exp x) = x
@@ -39,7 +83,7 @@ variable  exp_add    : ∀ x y, exp (x + y) = exp x * exp y
 include log_exp_eq exp_log_eq exp_pos exp_add
 
 example (x y z : real) :
-  exp (x + y + z) = exp x * exp y * exp z :=
+    exp (x + y + z) = exp x * exp y * exp z :=
 by rw [exp_add, exp_add]
 
 example (y : real) (h : y > 0)  : exp (log y) = y :=
@@ -47,4 +91,12 @@ exp_log_eq h
 
 theorem log_mul {x y : real} (hx : x > 0) (hy : y > 0) :
   log (x * y) = log x + log y :=
+sorry
+
+-- Exercise 7
+-- Prove the theorem below, using only the ring properties of ℤ enumerated in Section 4.2 and the theorem sub_self.
+
+#check sub_self
+
+example (x : ℤ) : x * 0 = 0 :=
 sorry
