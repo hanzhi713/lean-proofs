71f25c8be47b9b235ab43844475127f294c8e49d
hanzhi713 <hanzhi713@163.com>
2018-10-16 09:38:09 -0400

    Corrected Do_Twice

--- chapter_exercises/chap2_exercises.lean
+++ chapter_exercises/chap2_exercises.lean
@@ -1,12 +1,12 @@
 -- Exercise 1
 -- Define the function Do_Twice, as described in Section 2.4.
-def double_ (a: ℕ ) : ℕ := a + a
+def double_ (a : ℕ) : ℕ := a + a
 def double : ℕ → ℕ := λ x, x + x
 def square : ℕ → ℕ := λ x, x * x
 
 def times2plus1 : ℕ → ℕ := λ x, x * 2 + 1
 
-def do_twice_ (f: ℕ → ℕ ) (x: ℕ ) : ℕ := f (f x)
+def do_twice_ (f : ℕ → ℕ) (x : ℕ) : ℕ := f (f x)
 def do_twice : (ℕ → ℕ) → ℕ → ℕ := λ f x, f (f x)
 def do_third_times : (ℕ → ℕ) → ℕ → ℕ := λ f x, f (f (f x))
 
@@ -18,21 +18,20 @@ def quadruple : ℕ → ℕ := λ x, do_twice double x
 
 #reduce quadruple 2 -- 8
 
-def Do_Twice : ((ℕ → ℕ) → (ℕ → ℕ)) → (ℕ → ℕ) → ℕ → ℕ := λ f g x, f g (f g x)
-
+-- Do_Twice
 /- Equivalent forms -/
-def Do_Twice_ (f: (ℕ → ℕ) → (ℕ → ℕ)) : (ℕ → ℕ) → ℕ → ℕ := λ g x, f g (f g x)
-def Do_Twice' (f: (ℕ → ℕ) → (ℕ → ℕ)) (x: ℕ → ℕ) : ℕ → ℕ := f (f x)
-def Do_Twice'' : ((ℕ → ℕ) → (ℕ → ℕ)) → (ℕ → ℕ) → ℕ → ℕ := λ f g, f (f g)
+def Do_Twice (f : (ℕ → ℕ) → (ℕ → ℕ)) (x : ℕ → ℕ) : ℕ → ℕ := f (f x)
+def Do_Twice' : ((ℕ → ℕ) → (ℕ → ℕ)) → (ℕ → ℕ) → (ℕ → ℕ) := λ f g, f (f g)
 
-#reduce Do_Twice do_twice double 2
-#reduce Do_Twice do_twice times2plus1 2 
-#reduce Do_Twice do_third_times times2plus1 2
-#eval Do_Twice_ do_twice square 2
+#reduce Do_Twice do_twice double 2 -- 32
+#reduce Do_Twice do_twice times2plus1 2 -- 47
+#reduce Do_Twice do_third_times times2plus1 2 -- 1535
+#eval Do_Twice do_twice square 2 -- 65536
 
-#reduce do_third_times square 2
-#eval Do_Twice do_third_times square 2
-#eval Do_Twice do_twice square 2
+#reduce do_third_times square 2 -- 256
+#eval Do_Twice do_third_times square 2 -- 18446744073709551616
+#eval (((((2^2)^2)^2)^2)^2)^2 -- 18446744073709551616 
+#eval Do_Twice do_twice square 2 -- 65536
 
 
 -- Exercise 2
